415a92d5d8c80fc8dd5a7a259b53f7e832ecdf89
Alexey Zayats <alexis@agitum.ru>
2013-09-04 23:25:49 +0400

    sql for linux

--- share/db/linux/create.sh
+++ share/db/linux/create.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-# PGVERSION=`pg_config --version | awk {'print $2'} | awk -F '.' {'print $1"."$2'}`
-
-PGBINDIR=`pg_config --bindir`
-SHAREDIR=`pg_config --sharedir`
-PSQL=$PGBINDIR/psql
-
-export PGPASSFILE=pgpass
-export PGDATABASE=template1
-export PGPORT=5432
-export PGUSER=postgres
-export PGHOST=localhost
-export USER=melampig
+.  .pgenv
 
 echo createuser
-$PGBINDIR/psql -U $PGUSER -qf ../sql/create/user.sql
+$PSQL -U $USER -d $DB -qf ../sql/create/user.sql
 
 echo createdb
-$PGBINDIR/createdb -U $USER melampig
-
-export PGDATABASE=melampig
+$PSQL -U $USER -d $DB -qf ../sql/create/database.sql
 
 echo extentions
-$PGBINDIR/psql -U $PGUSER melampig -qf ../sql/create/extention.sql 
-$PGBINDIR/psql -U $PGUSER melampig -qf ../sql/create/cast.sql 
+$PSQL -U $USER -qf ../sql/create/extention.sql 
+$PSQL -U $USER -qf ../sql/create/cast.sql 
--- share/db/linux/init.sh
+++ share/db/linux/init.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-PGVERSION=`pg_config --version | awk {'print $2'} | awk -F '.' {'print $1"."$2'}`
+.  .pgenv
 
-PGBINDIR=`pg_config --bindir`
-SHAREDIR=`pg_config --sharedir`
-PSQL=$PGBINDIR/psql
-
-export PGPASSFILE=pgpass
-export PGDATABASE=melampig
-export PGPORT=5432
-export PGUSER=postgres
-export PGHOST=localhost
-export USER=melampig
-
-echo init
-$PSQL -U $USER -qf ../sql/init/drop.sql
+echo drop 
+$PSQL -qf ../sql/init/drop.sql
 
 echo functions
-$PSQL -U $USER -qf ../sql/init/functions/core.sql
-$PSQL -U $USER -qf ../sql/init/functions/melampig.sql
+$PSQL -qf ../sql/init/functions/core.sql
+$PSQL -qf ../sql/init/functions/build_fight_list.sql
+$PSQL -qf ../sql/init/functions/build_match_fights.sql
+$PSQL -qf ../sql/init/functions/build_match_list.sql
+$PSQL -qf ../sql/init/functions/fight_update_personal.sql
+$PSQL -qf ../sql/init/functions/fight_update_team.sql
+$PSQL -qf ../sql/init/functions/fight_log_after_update.sql
+$PSQL -qf ../sql/init/functions/generate_classement_points.sql
+$PSQL -qf ../sql/init/functions/build_fight_list.sql
+$PSQL -qf ../sql/init/functions/match_after_update.sql
+$PSQL -qf ../sql/init/functions/remove_competition_fight_match.sql
+$PSQL -qf ../sql/init/functions/wrestler_classement.sql	
 
 echo tables
-$PSQL -U $USER -qf ../sql/init/tables/core.sql
-$PSQL -U $USER -qf ../sql/init/tables/melampig.sql
+$PSQL -qf ../sql/init/tables/core.sql
+$PSQL -qf ../sql/init/tables/melampig.sql
 
 echo data
-$PSQL -U $USER -qf ../sql/init/data/melampig.sql
-$PSQL -U $USER -qf ../sql/init/data/geo.sql
+$PSQL -qf ../sql/init/data/melampig.sql
+$PSQL -qf ../sql/init/data/geo.sql
 
 echo indexes
-$PSQL -U $USER -qf ../sql/init/indexes.sql
+$PSQL -qf ../sql/init/indexes.sql
 
-echo test
-$PSQL -U $USER -qf ../sql/test/schema.sql
-$PSQL -U $USER -qf ../sql/test/firstname.sql
-$PSQL -U $USER -qf ../sql/test/lastname.sql
-$PSQL -U $USER -qf ../sql/test/generate.sql
-$PSQL -U $USER -qf ../sql/test/fio.sql
+echo views
+$PSQL -qf ../sql/init/views/v_fight_video.sql
+
